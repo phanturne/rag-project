8e526cb4708a9d63584a33f56ab7594e9ed73cbc
Kevin Ding <kding60@gatech.edu>
2024-05-27 21:41:47 -0700

    Remove traceloop observability (Error: Missing tiktoken_bg.wasm issue)

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,13 +1,15 @@
-export default function webpack(config, isServer) {
+// webpack config must be a function in NextJS that is used to patch the default webpack config provided by NextJS, see https://nextjs.org/docs/pages/api-reference/next-config-js/webpack
+export default function webpack(config) {
   config.resolve.fallback = {
     aws4: false,
   };
-  config.module.rules.push({
-    test: /\.node$/,
-    loader: "node-loader",
+
+  // Following lines will fix issues with onnxruntime-node when using pnpm
+  // See: https://github.com/vercel/next.js/issues/43433
+  config.externals.push({
+    "onnxruntime-node": "commonjs onnxruntime-node",
+    sharp: "commonjs sharp",
   });
-  if (isServer) {
-    config.ignoreWarnings = [{ module: /opentelemetry/ }];
-  }
+
   return config;
 }
